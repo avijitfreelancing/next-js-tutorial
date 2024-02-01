9b9591ca90c0db2d1ea7a1339146abc3b7635db2
samantaavijit <samantaavijit.2000@gmail.com>
2024-02-01 16:28:29 +0530

    changes

--- app/employee/employee-details/[[...id]]/page.jsx
+++ app/employee/employee-details/[[...id]]/page.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
 export default function EmployeeList({ params }) {
-  console.log("params", params);
-
   return <div>Employee-Details {params.id || "Not id Found"}</div>;
 }
